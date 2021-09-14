45857a6b3cbd684ff11c2a04b378c18646e76e1e
Alen <alenbasar@outlook.com>
2021-09-14 19:29:37 +1000

    made glowing subheaders

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -15,13 +15,23 @@ type Props = {
 const Home = (props: Props) => {
   const { content } = props;
   const { heading, subheading, cta, ctaLink } = content;
+  const s1 = subheading[0].split("");
+  const s2 = subheading[1].split("");
   return (
     <section className="c-home">
       <div className="c-home__banner">
         <h1 className="c-home__banner-heading">{heading}</h1>
-        {subheading.map((s) => (
-          <h1 className="c-home__banner-subheading">{s}</h1>
-        ))}
+        <div className="c-home__banner-subheading1">
+          {s1.map((item, index) => (
+            <span key={index}>{item}</span>
+          ))}
+        </div>
+        <div className="c-home__banner-subheading2">
+          {s2.map((item, index) => (
+            <span key={index}>{item}</span>
+          ))}
+        </div>
+
         <Link to={ctaLink} className="c-home__banner-contact-btn">
           {cta}
         </Link>
