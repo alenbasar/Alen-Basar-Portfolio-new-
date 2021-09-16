47884ea64e1913417092f6665890bd755accd920
Alen <alenbasar@outlook.com>
2021-09-16 18:17:46 +1000

    add CSSTransition

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
-import React, { HTMLAttributes } from "react";
+import React, { HTMLAttributes, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 export type BannerContent = {
   heading: string;
@@ -15,22 +16,35 @@ type Props = {
 const Home = (props: Props) => {
   const { content } = props;
   const { heading, subheading, cta, ctaLink } = content;
+  const [show, setShow] = useState(false);
+  useEffect(() => {
+    setShow(true);
+  }, []);
+
   const s1 = subheading[0].split("");
   const s2 = subheading[1].split("");
   const h = heading.split("");
   return (
     <section className="c-home">
       <div className="c-home__banner">
+        {/* <TransitionGroup> */}
         <h1 className="c-home__banner-hero">
           {h.map((item, index) => (
-            <span className="blast" key={index}>
-              {item === " " ? "\u00A0" : item}
-              {/* {item} */}
-            </span>
+            <CSSTransition
+              key={"csst" + index}
+              in={show}
+              timeout={300}
+              classNames="slide"
+            >
+              <span key={index}>
+                {item === " " ? "\u00A0" : item}
+                {/* {item} */}
+              </span>
+            </CSSTransition>
           ))}
           <br />
           {s1.map((item, index) => (
-            <span key={index} className="blast">
+            <span key={index}>
               {item === " " ? "\u00A0" : item}
               {/* {item} */}
             </span>
@@ -48,6 +62,15 @@ const Home = (props: Props) => {
         <Link to={ctaLink} className="c-home__banner-contact-btn">
           {cta}
         </Link>
+        {/* <button
+          onClick={() => {
+            setShow(!show);
+            console.log(show);
+          }}
+        >
+          Click me
+        </button> */}
+        {/* </TransitionGroup> */}
       </div>
       <div></div>
     </section>
